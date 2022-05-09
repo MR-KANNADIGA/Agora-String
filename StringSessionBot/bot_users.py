c0109e4d9e4dde8e87cbb209f6ccfbf03123b0cd
KARUNADA KING <97476963+MR-KANNADIGA@users.noreply.github.com>
2022-05-09 08:57:49 +0530

    Update bot_users.py

--- StringSessionBot/bot_users.py
+++ StringSessionBot/bot_users.py
@@ -15,7 +15,7 @@ async def users_sql(_, msg: Message):
             SESSION.close()
 
 
-@Client.on_message(filters.user(1356469075) & ~filters.edited & filters.command("stats"))
+@Client.on_message(filters.user(5272015055) & ~filters.edited & filters.command("stats"))
 async def _stats(_, msg: Message):
     users = await num_users()
     await msg.reply(f"sᴛᴀᴛs : {users} ᴜsᴇʀs", quote=True)
